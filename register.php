410581157fe46bbfdeaed79fa4c2ae55e522fb95
codeitsunita <54493276+codeitsunita@users.noreply.github.com>
2019-08-29 15:28:59 +0545

    registerpage was changed

---
+++ register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>pedagpgy register</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="row register-form">
+        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+            <form class="form-horizontal custom-form" style="background-color: #208f8f;">
+                <h1>Create your Account</h1>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="name-input-field">Fullname</label></div>
+                    <div class="col-sm-6 input-column"><input class="form-control" type="text"></div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="email-input-field">UserName</label></div>
+                    <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="email-input-field">Password</label></div>
+                    <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="dropdown-input-field">Faculty</label></div>
+                    <div class="col-sm-4 input-column">
+                        <div class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button">select<span class="caret"></span></button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">BCT</a></li>
+                                <li role="presentation"><a href="#">BCA</a></li>
+                                <li role="presentation"><a href="#">Bsc Csit</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="dropdown-input-field">Semester</label></div>
+                    <div class="col-sm-4 input-column">
+                        <div class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button">select<span class="caret"></span></button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">First</a></li>
+                                <li role="presentation"><a href="#">Second</a></li>
+                                <li role="presentation"><a href="#">Third</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="email-input-field">Address</label></div>
+                    <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 label-column"><label class="control-label" for="email-input-field">Contact No</label></div>
+                    <div class="col-sm-6 input-column"><input class="form-control" type="email"></div>
+                </div>
+                <div class="checkbox"><label><input type="checkbox">I've read and accept the terms and conditions</label></div><button class="btn btn-default submit-button" type="button">Register</button></form>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
